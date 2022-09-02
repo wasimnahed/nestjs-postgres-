52049b8968812a5f329f485ecf2c77203265b541
wasimnahed <Wasim.Nahed@softobiz.com>
2022-09-02 11:37:57 +0530

    i am wasim

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-
+// i am wasim nahed
 @Injectable()
 export class UserService {
 
